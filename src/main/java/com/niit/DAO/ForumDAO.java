2ad3e122d1b0fa43237c7f8d8138d74cd6e1dcac
NAGA AKHILA <NAGA AKHILA@LAPTOP-C1KQN4GL>
2017-09-09 14:10:52 +0530

    Blog,Blogcomment Backend Completed

---
+++ src/main/java/com/niit/DAO/ForumDAO.java
@@ -0,0 +1,15 @@
+package com.niit.DAO;
+import java.util.List;
+
+import com.niit.model.Blog;
+import com.niit.model.Forum;
+
+public interface ForumDAO {
+
+	public boolean createForum(Forum forum);
+	public Forum getForumdetails(int forumId);
+	public List<Forum> getForum();
+	public boolean approveForum(Forum forum);
+	public boolean editForum(int forumId);
+	public boolean deleteForum(int forumId);
+}
